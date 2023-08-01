116db7688aba03f9eaaa14225ed1057e97b8ad89
TonyTheShrimp <tony.ma.2006@outlook.com>
2023-08-01 10:25:09 -0400

    Button can trigger an animation (used elevator open as test)

--- Assets/Scripts/Button/Button.cs
+++ Assets/Scripts/Button/Button.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+    //public Animator animator;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
     }
 
     // Update is called once per frame
@@ -18,5 +18,6 @@ public class Button : MonoBehaviour
     public void OnMouseDown()
     {
         Debug.Log("yes");
+        //animator.SetBool("ElevatorOpen", true);
     }
 }
