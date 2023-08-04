103b0d5e9ea36af9707ffc0b72d671c12eaeeb84
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-08-03 20:20:16 -0500

    Temp No. 2.

---
+++ Assets/Scripts/Scene/PressToStart.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PressToStart : MonoBehaviour
+{
+    [SerializeField] private List<Color> gradient;
+    [SerializeField] private TextMeshPro textMesh;
+
+    void Start()
+    {
+        
+    }
+}
